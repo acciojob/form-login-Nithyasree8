c449b98f9b14aee334844ab23d2e36450fd2ff16
Nithyasree8 <113105796+Nithyasree8@users.noreply.github.com>
2024-09-17 21:38:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	const from =doccument.getElementById("from1";
+	const fromData =new FromData(from);
+	const firstName =fromData.get("fname");
+	const lastName =fromData.get("lname");
+	alert(firstName + "" +lastName)
+	
 
 }
